a69902b7fffd8c9f7c1123199fbcdf944b537262
Leandro N. Anjos <lnanjos@outlook.com>
2017-09-08 21:08:58 -0300

    Add domain class for secao

---
+++ src/main/java/br/com/WSValidyCheck/domain/Secao.java
@@ -0,0 +1,23 @@
+package br.com.WSValidyCheck.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@SuppressWarnings("serial")
+@Entity
+public class Secao extends GenericDomain {
+
+	@Column(length=50,nullable=false)
+	@Basic(optional=false)
+	private String nomeSecao;
+
+	public String getNomeSecao() {
+		return nomeSecao;
+	}
+
+	public void setNomeSecao(String nomeSecao) {
+		this.nomeSecao = nomeSecao;
+	}
+	
+}
